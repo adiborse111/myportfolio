107acfcd6ca667d505ff8a39b57d84e2adf757ed
adiborse111 <adi.borse111@gmail.com>
2024-08-22 22:12:12 +0530

    new commit

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map
+        ((item)=>(
+          <BentoGridItem 
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+          />
+        ))}
+      </BentoGrid>
+    </section>
   )
 }
 
